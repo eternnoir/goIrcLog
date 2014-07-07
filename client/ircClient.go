8d3ccba372a30d8d6683fafe8d5288c9ef09ed52
eternnoir <eternnoir@gmail.com>
2014-07-07 17:48:50 +0800

    add PRIVMSG

--- client/ircClient.go
+++ client/ircClient.go
@@ -23,14 +23,27 @@ func (c *ircClient) Connect() bool {
 	if err != nil {
 		fmt.Println("Can not Connect to " + serverStr)
 	}
-	c.connection.AddCallback("001", func(e *ircenv.Event) {
-        c.Join()
-        fmt.Println("Joined"+ c.channel)
-	})
+    c.setUpCallbacks()
 	c.connection.Loop()
 	return false
 }
 
-func (c *ircClient) Join(){
-    c.connection.Join(c.channel)
+func (c *ircClient) setUpCallbacks(){
+	c.connection.AddCallback("001", func(e *ircenv.Event) {
+		c.Join()
+		fmt.Println("Joined" + c.channel)
+	})
+    c.connection.AddCallback("PRIVMSG", func(e *ircenv.Event) {
+		c.ReceivedMag(e.Message())
+        fmt.Println(e.Message())
+	})
+
+}
+
+func (c *ircClient) Join() {
+	c.connection.Join(c.channel)
+}
+
+func (c *ircClient) ReceivedMag(msg string) {
+
 }
--- main.go
+++ main.go
@@ -1,7 +1,6 @@
 package main
 
 import(
-    "fmt"
     "./client"
 	//"crypto/tls"
 	//"testing"
